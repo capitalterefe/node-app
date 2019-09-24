022db1cbe00d044eefaa76a805f73688a585f42f
capital terefe <capital.terefe@gmail.com>
2019-09-24 16:00:56 -0400

    changed the port to 8080

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 
 const app = express();
-const port = process.env.PORT || "8000";
+const port = process.env.PORT || "8080";
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
